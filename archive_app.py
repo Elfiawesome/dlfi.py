67284c4d838851ce43e65a0a122ac4209ec1cea8
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-02-03 15:01:56 +0800

    refactor: Move logging setup to the top of the main function; add requirements file for dependencies

--- archive_app.py
+++ archive_app.py
@@ -2,8 +2,9 @@ from dlfi import DLFI
 from dlfi.logger import setup_logging
 import logging
 
+setup_logging(level = logging.DEBUG)
+
 def main() -> None:
-	setup_logging(level = logging.DEBUG)
 	dlfi = DLFI(".archive/my-archive", password="COMPLEX_PASSWORD")
 
 main()
